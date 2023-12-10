edbe717a4f35024bacf3e42ac3107d7a6d146e2a
Daniel <danielmesfin331@gmail.com>
2023-12-10 02:23:49 -0800

    all about the componet

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { ethers } from 'ethers';
+import FileUpload from './components/FileUpload.js';
+import Modal from './components/Modal.js';
+import Remove from './components/Remove.js';
 
+import './App.css';
+import DriveShare from "./artifacts/contracts/DriveShare.sol/DriveShare.json";
+import Display from "./components/Display.js";
 function App() {
+  const [account, setAccount] = useState("");
+  const [contract, setContract] = useState(null);
+  const [provider, setProvider] = useState(null);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [renvoke,setRenvoke]=useState(false);
+  useEffect(() => {
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    async function wallet() {
+      if (provider) {
+        await provider.send("eth_requestAccounts", []);
+        window.ethereum.on("chainChanged", () => {
+          window.location.reload();
+        });
+        const signer = await provider.getSigner();
+        const address = await signer.getAddress();
+        console.log(address);
+        setAccount(address);
+      // contract address
+        const contract = new ethers.Contract(
+          process.env.contractAddress,
+          DriveShare.abi,
+          signer
+        );
+        console.log("contact is deployed on ");
+        console.log(contract);
+        setContract(contract);
+        setProvider(signer);
+      } else {
+        alert("Mask is not available");
+      }
+    }
+    provider && wallet();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <>
+      {!modalOpen && (
+        <button className="share" onClick={() => setModalOpen(true)}>
+          Share
+        </button>
+      )}
+      {modalOpen && (
+        <Modal setModalOpen={setModalOpen} contract={contract}></Modal>
+      )}
+      {!renvoke && (
+        <button className="share" onClick={() => setRenvoke(true)}>
+          Revoke
+        </button>
+      )}
+      {renvoke && (
+        <Remove setRenvoke={setRenvoke} contract={contract}></Remove>
+      )}
+      <div className="App" >
+        <h1 style={{ color: "white" }}>Ethio Drive 0.1</h1>
+        <div class="bg"></div>
+        <div class="bg bg2"></div>
+        <div class="bg bg3"></div>
+
+        <p style={{ color: "white" }}>
+          Account : {account ? account : "Not connected"}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <FileUpload account={account} contract={contract} />
+        <Display account={account} contract={contract} />
+      </div>
+    </>
+
   );
 }
 
